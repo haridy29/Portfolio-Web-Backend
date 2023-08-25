fd9c7363c51416b894c5f697d6f6afd1f25908d7
haridy29 <mohamed.haridi.14@gmail.com>
2023-08-26 01:01:50 +0300

    Delete unneeded api

--- src/main/java/com/example/Portfolio/controllers/AdminController.java
+++ src/main/java/com/example/Portfolio/controllers/AdminController.java
@@ -1,22 +1,6 @@
 package com.example.Portfolio.controllers;
 
-import com.example.Portfolio.entities.Article;
-import com.example.Portfolio.services.ArticleService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-@RestController("/")
 public class AdminController {
 
-    public final ArticleService articleService;
-
-    public AdminController(ArticleService articleService) {
-        this.articleService = articleService;
-    }
-
-    @GetMapping("admin/addArticle/{title}/{blog}")
-    public Article addAreticle(@PathVariable("title") String title, @PathVariable("blog") String blog) {
-        return articleService.addArticle(title, blog);
-    }
 
 }
