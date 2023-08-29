4a62495050a2f4bf5690e7bfb6735b12b67b0304
haridy29 <mohamed.haridi.14@gmail.com>
2023-08-29 23:35:42 +0300

    restore the deleted command for update profile feature

--- src/main/java/com/example/Portfolio/controllers/AdminController.java
+++ src/main/java/com/example/Portfolio/controllers/AdminController.java
@@ -1,10 +1,8 @@
 package com.example.Portfolio.controllers;
 
-import com.example.Portfolio.dtos.ArticleDTO;
 import com.example.Portfolio.dtos.ProfileDTO;
 import com.example.Portfolio.services.ArticleService;
 import com.example.Portfolio.services.ProfileService;
-import com.example.Portfolio.views.ArticleView;
 import com.example.Portfolio.views.ProfileView;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,7 +22,7 @@ public class AdminController {
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello from secured endpoint.");
     }
-}
+
     public ResponseEntity<String> updateProfile(Principal principal, @Valid @RequestBody ProfileDTO profile) {
         return profileService.updateProfile(principal, profile);
     }
